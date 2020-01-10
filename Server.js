7a841859d299fca2e3586e7ed0a371b3317e60b5
Olabanji121 <olabanjiabimbola@gmail.com>
2020-01-10 20:18:42 +0100

    update

--- Server.js
+++ Server.js
@@ -21,14 +21,13 @@ app.use('/api/v1/users', userRoute)
 
 // Sever Static Assets  in production
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'))
+    app.use(express.static(path.join(__dirname, 'client/build')))
 
-    app.get('*', (req,res)=> res.sendFile(path.resolve(__dirname, 'client', "build", 'index.html')));
-}
+    app.get('/*', (req,res)=> {res.sendFile(path.resolve(__dirname, '/client/build', 'index.html'))})
 
 
-const port  = process.env.PORTBE || 7070
+
+const port  = process.env.PORT || 7070
 
 app.listen(port,  ()=>{
     console.log(`server started on port ${port}`);
