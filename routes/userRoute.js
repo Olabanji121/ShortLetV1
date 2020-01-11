8ab0623561ace2ed6ba6c5de90c4180ef9045f68
Olabanji121 <olabanjiabimbola@gmail.com>
2020-01-11 11:05:48 +0100

    userRoute

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -6,7 +6,7 @@ const router = express.Router()
 
 router.post('/signup', authController.signup)
 router.post('/login', authController.login)    
-// router.get('/getloginUser', authController.protect, authController.getUser)    
+router.get('/getloginUser', authController.protect, authController.getUser)    
 
 
 module.exports = router;    
